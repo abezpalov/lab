5f20ade4db64b83b5a924716eb8fd44cc5506004
Andrey Bezpalov <abezpalov@ya.ru>
2024-03-11 13:40:25 +0400

    Update main.py

--- 2024_lang_python/main.py
+++ 2024_lang_python/main.py
@@ -130,26 +130,114 @@ def lab_1d():
 
 
 def lab_1e():
+
+    # Читаем количество дней и избавляемся от этих данных
     _ = input()
     del _
-
-    count_of_rec = 0
-    n_bad = 0
-    max_bad = 0
 
     ok = {'+': True,
           '-': False}
 
+    days = input().strip()
 
+    good_rec = 0
+    n_of_bad = 0
+    max_n_of_bad = 0
+    rec_of_max_bad = -1
+    prev_day = False
+
+    for day in days:
+
+        #Если рецепт приготовлен не удачно
+        if not ok[day]:
+            n_of_bad += 1
+            if n_of_bad > max_n_of_bad:
+                max_n_of_bad = n_of_bad
+                rec_of_max_bad = good_rec + 1
+            prev_day = ok[day]
+
+        else:
+            if prev_day:
+                good_rec += 1
+                n_of_bad = 0
+                prev_day = False
+
+            else:
+                prev_day = True
+
+    print(good_rec)
+    print(rec_of_max_bad, max_n_of_bad)
+
+
+# TODO Не проходит первый тест
+def lab_1f():
+
+    # Читаем количество дней и избавляемся от этих данных
+    _ = input()
+    del _
+
+    ok = {'+': True,
+          '-': False}
+
+    current_diff = 1
+
+    max_diff = 0
+    min_diff = 1
+
+    n_of_min_diff = 1
+    n_of_max_diff = 1
 
     days = input().strip()
-    for day in days:
-       pass
+    for n, day in enumerate(days):
 
+        if ok[day]:
+            current_diff += 1
+        else:
+            current_diff -= 1
+
+        if current_diff > max_diff:
+            max_diff = current_diff
+            n_of_max_diff = n+2
+
+        if current_diff < min_diff:
+            min_diff = current_diff
+            n_of_min_diff = n+2
+
+    first_diff = 1 if min_diff > 1 else 1-min_diff
+
+    print(first_diff)
+    print(n_of_min_diff)
+    print(n_of_max_diff)
+
+
+def lab_2a():
+
+    n_of_urls, time_limit = input().strip().split()
+    n_of_urls, time_limit = int(n_of_urls), int(time_limit)
+    time_current = 0
+
+    urls = input().strip().split()
+
+    for n, e in enumerate(urls):
+        urls[n] = int(e)
+
+    urls.sort(reverse=True)
+
+    while time_limit >= time_current and len(urls):
+
+        i = 0
+        while urls[i] == urls[0] and i < len(urls):
+            i += 1
+
+        time_current += urls[0] * i
+        urls = urls[i:]
+
+    print(n_of_urls - len(urls))
+    print(time_current)
 
 
 # Настройки
-LAB_N = '1d'
+LAB_N = '2a'
 
 labs = {
     '1a': lab_1a,
@@ -157,6 +245,8 @@ labs = {
     '1c': lab_1c,
     '1d': lab_1d,
     '1e': lab_1e,
+    '1f': lab_1f,
+    '2a': lab_2a,
 }
 
 
